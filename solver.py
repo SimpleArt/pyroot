4faef3c7b53ca04070b02e924d37622379f2e871
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2020-10-23 10:35:44 -0400

    Geometric bisection added

--- solver.py
+++ solver.py
@@ -5,12 +5,56 @@ from Bracketing_Methods.brent            import brent
 from Bracketing_Methods.newton_quadratic import newton_quadratic
 from Bracketing_Methods.chandrupatla     import chandrupatla
 
+from math import sqrt
+import sys
+realmin = sys.float_info.min  # Smallest positive float
+realmax = sys.float_info.max  # Largest positive float
+
 def is_nan(x):
     return x != x
+
 def is_infinite(x):
     return abs(x) == float('inf')
 
-print_flag = False
+def sign(x):
+    """Returns the sign of x:
+    if x > 0: +1
+    if x < 0: -1
+    if x == 0: 0
+    """
+    return (x>0) - (x<0)
+
+def mean(x, y, bisection_flag):
+    """Alternates between using
+    - arithmetic means and
+    - geometric means
+    to guarantee fast travel
+    through all possible floats."""
+    
+    # Sort x, y
+    if abs(x) > abs(y):
+        x, y = y, x
+    
+    # Try arithmetic mean
+    if bisection_flag:
+        if abs(y) < 1e300: return 0.5*(x+y)
+        # Overflow
+        return 0.5*x+0.5*y
+    
+    # If geometric mean should be tried
+    # but there is a sign issue, then
+    # replace the smaller of x and y with
+    # the smallest nonzero float times
+    # the sign of the larger of x and y.
+    if sign(x) != sign(y):
+        x = sign(y)*realmin
+    if abs(x) > 1e-150 and abs(y) < 1e150:
+        return sign(x)*sqrt(x*y)
+    # Under-/Over- Flow
+    return sign(x)*sqrt(abs(x))*sqrt(abs(y))
+
+print_iterations = False
+print_result     = False
 
 bracketing_method_dict = {
     'bisection'      : bisection,
@@ -40,7 +84,7 @@ solver.root_in(f, x1, x2, {method, iterations, error, f1, f2})
 
 @defaults
     method: 'chandrupatla'
-    iterations: 1000
+    iterations: 200
     error: 1e-14
 
 Attempts to solve f(x) = 0 between x1 and x2, given f(x1) and f(x2) have different signs, where convergence to a root can be guaranteed using binary search, a.k.a. bisection.
@@ -66,7 +110,7 @@ solver.optimize(g, x1, x2, {method, iterations, error, f})
 @defaults
     f: g(x+dx) - g(x-dx), where dx = error + 1e-8*abs(x)
     method: 'chandrupatla'
-    iterations: 1000
+    iterations: 200
     error: 1e-8
 
 Attempts to find extreme values of g(x) between x1 and x2, given g(x) is increasing at one point and decreasing at the other. Uses solver.root_in to find the root of f(x), the derivative of g(x).
@@ -126,11 +170,6 @@ Use solver.help('root_in'), solver.help('optimize'), or solver.help('methods') f
 """)
 
 
-def sign(x):
-    """Returns the sign of x"""
-    return (x>0) - (x<0)
-
-
 def bracketing_method(x1, f1, x2, f2, x3, f3, x4, f4, t):
     """Interface for the bracketing method.
     
@@ -170,25 +209,28 @@ def root_in(f, x1, x2,
         bracketing_method = chandrupatla
     
     """Set default iterations and error"""
-    if iterations is None: iterations = 1000
+    if iterations is None: iterations = 200
     if error is None: error = 1e-16
     
     """Compute initial points"""
     if x1 == x2: return x1
+    if is_infinite(x1):
+        x1 = sign(x1)*realmax
+    if is_infinite(x2):
+        x2 = sign(x2)*realmax
     if f1 is None: f1 = f(x1)
     if f2 is None: f2 = f(x2)
     if is_nan(f1) or is_nan(f2): return 0.5*(x1+x2)
     if f1 == 0: return x1
     if f2 == 0: return x2
     if sign(f1) == sign(f2): return (f2*x1-f1*x2)/(f2-f1)
-    if is_infinite(x2):
-        x1, f1, x2, f2 = x2, f2, x1, f1
     
     """Initialize variables"""
     n = 0
     bisection_fails = 0
     x3, f3 = None, None
     t = 0.5 # Safe start is bisection
+    bisection_flag = True
     
     """Loop until convergence"""
     while (is_infinite(x1) or abs(x1-x2) > error+5e-15*abs(x1+x2)) and f2 != 0 and not is_nan(f2):
@@ -199,33 +241,30 @@ def root_in(f, x1, x2,
         
         """Compute next point"""
         
-        # One side is infinite, set x = +/-1
-        # on the side which is infinite and
-        # then apply an exponential search.
-        if is_infinite(x1):
-            t = 0.5
-            if sign(x1)*x2 < 1: x = float(sign(x1))
-            else: x = 2*x2
-        
         # Overflow
-        elif is_infinite(x1-x2):
+        if is_infinite(x1-x2):
             t = 0.5
             x = 0.0
         
-        # Both bounds are finite and overflow
-        # does not occur.
+        # Overflow does not occur.
         # 1. If f(x) is infinite, use bisection.
         # 2. Compute the next point.
+        # 2.1. Alternate between arithmetic and geometric mean,
+        # 2.2. or just use x = x2 + t*(x1-x2).
         # 3. Apply tolerance.
         else:
             if is_infinite(f1) or is_infinite(f2): t = 0.5
-            x = x2 + t*(x1-x2)
+            if t == 0.5:
+                x = mean(x1, x2, bisection_flag)
+                bisection_flag = not bisection_flag
+            else:
+                x = x2 + t*(x1-x2)
             x += 0.25*(error+5e-15*abs(x1+x2))*sign(t-0.5)*sign(x2-x1)
         
         fx = f(x)
         
         """======For seeing iterations======"""
-        if print_flag: print(f'f({x}) \t= {fx}')
+        if print_iterations: print(f'f({x}) \t= {fx}')
         
         """Swap to ensure x replaces x2"""
         if sign(f1) == sign(fx):
@@ -238,7 +277,7 @@ def root_in(f, x1, x2,
         
         """Update counters"""
         n += 1
-        if t < 0.5: bisection_fails += 1
+        if t < 0.5 or (x1-x2)/(x1-x3) > 0.75: bisection_fails += 1
         else: bisection_fails = 0
         
         """Compute t for next iteration"""
@@ -261,16 +300,17 @@ def root_in(f, x1, x2,
             # Resort to bisection if over-stepped too much
             t = min(0.5, temp)
         
-        # Resort to bisection if bounds are
-        # not closing in after 4 iterations
-        # or the result is out of bounds.
-        if bisection_fails == 4 or t >= 1 or t <= 0 or is_nan(t):
+        # Resort to bisection if
+        # - interval failed to halve after a corrective adjustment
+        # - t is out of bounds
+        if bisection_fails > 3 or t >= 1 or t <= 0 or is_nan(t):
             t = 0.5
     
-    """======For seeing iterations======"""
-    if print_flag: print(f'{n}th iteration: {(x1*f2-x2*f1)/(f2-f1)}')
+    """======For seeing final result======"""
+    if print_result: print(f'{n}th iteration: {(x1*f2-x2*f1)/(f2-f1)}')
     
     """Return secant iteration"""
+    if f2 == 0: return x2
     return (x1*f2-x2*f1)/(f2-f1)
 
 def optimize(g, x1, x2,
@@ -290,7 +330,7 @@ def optimize(g, x1, x2,
     solver.bracket(f, x1, x2, method, iterations, error) is used to find the root.
     """
     
-    """Set default iterations and error"""
+    """Set default error"""
     if error is None: error = 1e-8
     
     """Symmetric difference"""
@@ -304,7 +344,7 @@ def optimize(g, x1, x2,
     
     x = root_in(f, x1, x2, method, iterations, error)
     """======For seeing iterations======"""
-    if print_flag: print(f'\ng({x}) \t= {g(x)}')
+    if print_result: print(f'\ng({x}) \t= {g(x)}')
     return x
 
 
